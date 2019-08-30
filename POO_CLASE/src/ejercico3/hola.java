aff9043eef380875105713fdc7f0db7e651d49d3
Sataroto <Alumno@LAB02-PC01>
2019-08-30 11:34:31 -0500

    Hola Señor Locutor

---
+++ POO_CLASE/src/ejercico3/hola.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercico3;
+
+/**
+ *
+ * @author Alumno
+ */
+public class hola {
+    public static void main(String[] aggg){
+        ConvertidorCadena carac = new ConvertidorCadena("hola");
+        carac.
+    }
+    
+}
