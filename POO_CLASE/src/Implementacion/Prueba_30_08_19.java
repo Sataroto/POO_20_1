515916f2cc1fa4df9528b12840bb51d624ab3133
Sataroto <>
2019-08-30 10:18:18 -0500

    Prueba de Hoy antes de iniciar

---
+++ POO_CLASE/src/Implementacion/Prueba_30_08_19.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Implementacion;
+
+import Clases.Funciona;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Prueba_30_08_19 {
+    
+    public static void main (String[] gggg){
+        Funciona ejemplo = new Funciona();
+        System.out.println(ejemplo.funcion());
+        System.out.println("hola");
+    }
+}
